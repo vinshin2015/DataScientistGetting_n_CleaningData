f9b0d6e1d834f2125cc217955983bab15df37a72
vinshin2015 <vinayak.shinde@gmail.com>
2015-10-25 16:26:07 +0530

    Corrected code to move merge towards the last step

--- run_analysis.R
+++ run_analysis.R
@@ -7,57 +7,62 @@ rownames<-t(read.table("./features.txt"))
 
 #Read the 6 Activities as a table with activity code and activity long name
 activity<-read.table("./activity_labels.txt")
-
+names(activity)<-c("ActivityCd","ActivityLabel")
 #Test data preparation
 x_test<-read.table("./test/X_test.txt") 
 #Assign column/Variable names
 names(x_test)<-rownames[2,]
 #Activities
 y_test<-read.table("./test/y_test.txt")
+names(y_test)<-c("ActivityCd")
 #Join/Merge with Activity description/label.
-y_test_activity<-merge(y_test,activity,by.x = "V1", by.y = "V1", all.x = TRUE)
-names(y_test_activity)<-c("ActivityCd","ActivityLabel")
+#y_test_activity<-merge(y_test,activity,by.x = "V1", by.y = "V1", all.x = TRUE)
+#names(y_test_activity)<-c("ActivityCd","ActivityLabel")
 #Subjects
 sub_test<-read.table("./test/subject_test.txt")
 names(sub_test)<-c("Subject")
 
 #Create a complete Test data set by ensuring that the subject,
 #activity and the measuresments are combined
-CompleteTestDataset<-cbind(sub_test,y_test_activity,x_test)
+CompleteTestDataset<-cbind(sub_test,y_test,x_test)
 
 #Training data preparations
 x_train<-read.table("./train/X_train.txt") 
 names(x_train)<-rownames[2,]
 #Activities
 y_train<-read.table("./train/y_train.txt")
+names(y_train)<-("ActivityCd")
 #Join/Merge with Activity description/label.
-y_train_activity<-merge(y_train,activity,by.x = "V1", by.y = "V1", all.x = TRUE)
-names(y_train_activity)<-c("ActivityCd","ActivityLabel")
+#y_train_activity<-merge(y_train,activity,by.x = "V1", by.y = "V1", all.x = TRUE)
+#names(y_train_activity)<-c("ActivityCd","ActivityLabel")
 #Subjects
 sub_train<-read.table("./train/subject_train.txt")
 names(sub_train)<-c("Subject")
 
 #Create a complete Train data set by ensuring that the subject,
 #activity and the measuresments are combined
-CompleteTrainingDataset<-cbind(sub_train,y_train_activity,x_train)
+CompleteTrainingDataset<-cbind(sub_train,y_train,x_train)
 
 #Trainign and Test combined
 MergedTrainingTest<-rbind(CompleteTrainingDataset,CompleteTestDataset)
-
+#MergedTrainingTest<-merge(MergedTrainingTest1,activity,by.x = "ActivityCd", by.y = "ActivityCd", all.x = TRUE)
 library(dplyr)
 #subsetting the data set for mean and std values
 TmpNames<-names(MergedTrainingTest)
 #Finding indices of the columsn for mean and std deviations
-MeanIndex<-grep("mean",TmpNames)
-StdIndex<-grep("std",TmpNames)
+MeanIndex<-grep("mean()",TmpNames, fixed = TRUE)
+StdIndex<-grep("std()",TmpNames, fixed = TRUE)
 #Creating a list of cols to retain
-ColToRetain<-c(1,2,3,MeanIndex,StdIndex)
+ColToRetain<-c(1,2,MeanIndex,StdIndex)
 #subsetting the data frame to retain cols of intrest
 SubMergedTrainingTest<-MergedTrainingTest[,ColToRetain]
 
 #Calculating Mean
-FinalAverages<-SubMergedTrainingTest %>% group_by(Subject,ActivityCd,ActivityLabel) %>% summarize_each(funs(mean))
-
+FinalAverages<-SubMergedTrainingTest %>% group_by(Subject,ActivityCd) %>% summarize_each(funs(mean))
+#Attach activity variables.
+Outputdataset<-merge(activity,FinalAverages,by.x = "ActivityCd", by.y = "ActivityCd", all.x = TRUE)
+Outputdataset<-Outputdataset[2:69]
+Outputdataset<-arrange(Outputdataset,ActivityLabel,Subject)
 # Write file
-write.table(FinalAverages, file = "FinalAverages.txt", row.names = FALSE)
+write.table(Outputdataset, file = "Outputdataset.txt", row.names = FALSE)
 
